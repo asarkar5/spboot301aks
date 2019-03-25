cf0aba071230944242247493d07cdfd789368d27
asarkar <asarkar@promnetwork.com>
2019-03-25 14:38:20 -0400

    Initial commit

---
+++ src/main/java/com/springboot_301aks/spboot301aks/Spboot301aksApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_301aks.spboot301aks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spboot301aksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spboot301aksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot_301aks/spboot301aks/Spboot301aksApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot_301aks.spboot301aks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spboot301aksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
